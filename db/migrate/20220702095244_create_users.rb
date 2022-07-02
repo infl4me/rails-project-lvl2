64a15320c2376af6fef3ba8706b38d0f9dc50fd0
udf <hellpl4y@gmail.com>
2022-07-02 15:32:49 +0500

    gen devise model

--- db/migrate/20220702095244_create_users.rb
+++ db/migrate/20220702095244_create_users.rb
@@ -1,9 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.email :string
-      t.first_name :string
-      t.last_name :string
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
