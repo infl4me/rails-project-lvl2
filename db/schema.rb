e5662ad86442056e9cb83d2f9833aa3682e5816f
udf <hellpl4y@gmail.com>
2022-07-06 23:38:50 +0500

    add category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_095621) do
+ActiveRecord::Schema.define(version: 2022_07_06_183053) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+%w[development business ruby js job].each do |category|
+  Category.create(name: category)
+end
