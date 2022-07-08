06a9ac0d42d5f126bf8370b2bce366c80927ac54
udf <hellpl4y@gmail.com>
2022-07-08 18:12:41 +0500

    fix tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @post = posts(:one)
+    @user = users(:one)
+    sign_in @user
   end
 
   test 'should get index' do
