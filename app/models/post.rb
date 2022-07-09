235fec8b0b67bbb42de2ab82285fb29507818fb2
udf <hellpl4y@gmail.com>
2022-07-09 23:36:03 +0500

    add PostComment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :creator, class_name: 'User', inverse_of: :posts
   belongs_to :category
+  has_many :post_comments, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true
