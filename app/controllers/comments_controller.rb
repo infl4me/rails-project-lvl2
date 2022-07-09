861a702cb6fabe38a04f8dfdb228de31430b0361
udf <hellpl4y@gmail.com>
2022-07-10 01:44:29 +0500

    add tests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,11 +13,6 @@ class CommentsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_comment
-    @comment = PostComment.find(params[:id])
-  end
-
   # Only allow a list of trusted parameters through.
   def comment_params
     params.require(:post_comment).permit(:content)
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,48 +1,17 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @comment = comments(:one)
-  end
-
-  test 'should get index' do
-    get comments_url
-    assert_response :success
-  end
-
-  test 'should get new' do
-    get new_comment_url
-    assert_response :success
+    sign_in users(:one)
   end
 
   test 'should create comment' do
-    assert_difference('Comment.count') do
-      post comments_url, params: { comment: {} }
+    assert_difference('PostComment.count') do
+      post post_comments_path(posts(:one)), params: { post_comment: { content: 'blablabla' } }
     end
 
-    assert_redirected_to comment_url(Comment.last)
-  end
-
-  test 'should show comment' do
-    get comment_url(@comment)
-    assert_response :success
-  end
-
-  test 'should get edit' do
-    get edit_comment_url(@comment)
-    assert_response :success
-  end
-
-  test 'should update comment' do
-    patch comment_url(@comment), params: { comment: {} }
-    assert_redirected_to comment_url(@comment)
-  end
-
-  test 'should destroy comment' do
-    assert_difference('Comment.count', -1) do
-      delete comment_url(@comment)
-    end
-
-    assert_redirected_to comments_url
+    assert_redirected_to post_path(posts(:one))
   end
 end
