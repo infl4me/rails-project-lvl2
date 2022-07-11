828eb9bd3795a73e50acec634939bc058327c9c9
udf <hellpl4y@gmail.com>
2022-07-12 00:04:50 +0500

    fix tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -12,6 +12,16 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
       post post_comments_path(posts(:one)), params: { post_comment: { content: 'blablabla' } }
     end
 
-    assert_redirected_to post_path(posts(:one))
+    assert_redirected_to(/#{post_path(posts(:one))}/)
+  end
+
+  test 'should create child comment' do
+    assert_difference('PostComment.count') do
+      post post_comments_path(posts(:one)), params: { post_comment: { content: 'blablabla', parent_id: post_comments(:one).id } }
+    end
+
+    assert_equal(post_comments(:one), PostComment.last.parent)
+
+    assert_redirected_to(/#{post_path(posts(:one))}/)
   end
 end
