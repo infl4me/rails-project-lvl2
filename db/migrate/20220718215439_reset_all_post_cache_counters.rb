1387e2175708c658c0144c1274d69a73633f47b9
udf <hellpl4y@gmail.com>
2022-07-19 04:55:34 +0500

    fix

--- db/migrate/20220718215439_reset_all_post_cache_counters.rb
+++ db/migrate/20220718215439_reset_all_post_cache_counters.rb
@@ -3,7 +3,7 @@ class ResetAllPostCacheCounters < ActiveRecord::Migration[6.1]
     def up
       Post.all.each do |post|
         Post.reset_counters(post.id, :likes)
-        end
       end
+    end
   end
 end
